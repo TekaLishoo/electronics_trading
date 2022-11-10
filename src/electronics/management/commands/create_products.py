8e2671cd25519542ceb8ef1a705ce0f067c6192f
TekaLishoo <teka.lishoo@gmail.com>
2022-11-10 19:19:18 +0300

    products added to database

---
+++ src/electronics/management/commands/create_products.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from src.electronics.models import Product
+from src.core.choices import TYPE_OF_PRODUCT
+from random import choice, randint
+from faker import Faker
+from datetime import date, timedelta
+
+
+class Command(BaseCommand):
+    help = "Create a given number of random products"
+
+    def add_arguments(self, parser):
+        parser.add_argument("amount", nargs="+", type=int)
+
+    def handle(self, *args, **options):
+        for n in range(*options["amount"]):
+            product = Product()
+
+            product.name = choice(TYPE_OF_PRODUCT)[0]
+            product.model = f'{chr(randint(ord("A"), ord("Z")))}{choice(range(100, 900))}'
+            product.presentation_data = date.today() + timedelta(days=randint(1, 20))
+
+            product.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Successfully created %s products" % int(*options["amount"])
+            )
+        )
